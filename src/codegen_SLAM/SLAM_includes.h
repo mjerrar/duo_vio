bd435c65ac231751b9d0f52fbf77af89055a509c
depnicol <depnicol@student.ethz.ch>
2015-08-14 16:47:17 +0200

    coder

---
+++ src/codegen_SLAM/SLAM_includes.h
@@ -0,0 +1,10 @@
+#ifndef __SLAM_INCLUDES__
+#define __SLAM_INCLUDES__
+#include <rtGetInf.h>
+#include <rtGetNaN.h>
+#include <rt_nonfinite.h>
+#include <rtwtypes.h>
+#include <SLAM.h>
+#include <SLAM_includes.h>
+#include <SLAM_types.h>
+#endif
